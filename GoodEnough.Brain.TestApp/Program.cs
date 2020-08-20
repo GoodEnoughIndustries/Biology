1bf6244cc7748ebbf51f316480ec3462e21df66c
Chad Jessup <chad.jessup@gmail.com>
2020-08-19 21:46:41 -0700

    initial commit

---
+++ GoodEnough.Brain.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoodEnough.Brain.TestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
